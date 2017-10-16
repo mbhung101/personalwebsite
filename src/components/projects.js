b08f635482f6788414f52cef526ab3838781d1b0
github username <github email address>
2017-10-16 17:35:10 -0400

    project template and pictures:

--- src/components/projects.js
+++ src/components/projects.js
@@ -8,9 +8,33 @@ export default class Projects extends Component {
   }
   render(){
     return(
-      <div>
+        <div>
         <Nav/>
-        Projects
+        <h1 align="center"> Sprout </h1>
+        <hr></hr>
+        <div  className="ui text container">
+          <font size="4"> Sprout is an app for anyone who grows plants! It can make a garden, save and track your vegetable harvests and compare your grows with other users.</font>
+        </div>
+        <br></br>
+        <div align="center">
+          <img style={{width:900,height:500}} src={require('./imgs/sc1.png')} />
+        </div>
+        <br></br>
+        <div  className="ui text container">
+          <font size="4"> A user profile contains changeable basic information and profile picture. From the home page, they can utilize the one of many the built in functionalities, like alerts and weather forecaster. </font>
+        </div>
+        <br></br>
+        <div align="center">
+          <img style={{width:900,height:500}} src={require('./imgs/sc2.png')} />
+        </div>
+        <br></br>
+        <div  className="ui text container">
+          <font size="4"> Users can search for a the conditions of vegetables grown by others and accompanying data for when they harvested them to see when to expect thiers or get ideas on how to improve the next batch!  </font>
+        </div>
+        <a href= "https://github.com/mbhung101/SproutFront"> Frontend Link </a>
+        <br></br>
+        <a href= "https://github.com/mbhung101/SproutBack"> Backend Link </a>
+        <br></br>
       </div>
     )
   }
--- src/components/resume.js
+++ src/components/resume.js
@@ -10,7 +10,9 @@ export default class Resume extends Component {
     return(
       <div>
         <Nav/>
-        Resume
+        <h2> Employment History </h2>
+        <hr></hr>
+        <img style={{width:250,height:250}} src={require('./imgs/mts.jpg')} />
       </div>
     )
   }
