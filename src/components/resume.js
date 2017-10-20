e20da04f04733d0e7e309ae57136d4ed7ee672e2
github username <github email address>
2017-10-20 11:21:34 -0400

    education

--- src/components/resume.js
+++ src/components/resume.js
@@ -7,7 +7,7 @@ export default class Resume extends Component {
     return(
     <div>
       <Nav/>
-      <h2> Comfortable With: </h2>
+      <h2> Comfortable With </h2>
       <hr></hr>
 
       <div className="rows">
@@ -80,14 +80,86 @@ export default class Resume extends Component {
 
         <div className="row">
           <div className="resumeContainer" style={{height:150,width:150,padding:10}}>
-            <img className="resumeImage" style={{height:150,width:150,padding:10}} src={require('./imgs/SQL1.png')} />
+            <img className="resumeImage" style={{height:150,width:150,padding:10}} src={require('./imgs/jq.gif')} />
             <div className="overlay">
-              <div className="overlayText">SQL</div>
+              <div className="overlayText">JQuery</div>
             </div>
           </div>
         </div>
-        
+
+        <div className="row">
+          <div className="resumeContainer" style={{height:150,width:150,padding:10}}>
+            <img className="resumeImage" style={{height:150,width:150,padding:10}} src={require('./imgs/pg.png')} />
+            <div className="overlay">
+              <div className="overlayText">Postgres</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="resumeContainer" style={{height:150,width:150,padding:10}}>
+            <img className="resumeImage" style={{height:150,width:150,padding:10}} src={require('./imgs/redux.png')} />
+            <div className="overlay">
+              <div className="overlayText">Redux</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="resumeContainer" style={{height:150,width:150,padding:10}}>
+            <img className="resumeImage" style={{height:150,width:150,padding:10}} src={require('./imgs/sin.png')} />
+            <div className="overlay">
+              <div className="overlayText">Sinatra</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="resumeContainer" style={{height:150,width:150,padding:10}}>
+            <img className="resumeImage" style={{height:150,width:150,padding:10}} src={require('./imgs/ajax-logo.png')} />
+            <div className="overlay">
+              <div className="overlayText">Ajax</div>
+            </div>
+          </div>
+        </div>
+
       </div>
+
+      <h2> Education </h2>
+      <hr></hr>
+      <br></br>
+      <div style={{display:"inline-block",padding:15}}>
+        <img style={{height:200,width:300}} src={require('./imgs/flatiron_gif.gif')} />
+      </div>
+      <div className="ehBox" style={{display:"inline-block",paddingLeft:15, paddingTop:50}}>
+         <font size="4"> 040317 "Full Metal Brackets" Cohort </font>
+         <br></br>
+         Software Engineer Immersive Program
+         <hr></hr>
+         <ul>
+          <li> Learned popular web technologies in intensive and selective 15 week program </li>
+          <li> Accelerated curriculum instilled rapid acclimatization to new languages and new technologies </li>
+          <li> Emphasis on deadlined, low oversight, collaborative projects  </li>
+         </ul>
+      </div>
+
+      <div style={{display:"inline-block",padding:15}}>
+        <img style={{height:200,width:300}} src={require('./imgs/uwm2.jpg')} />
+      </div>
+      <div className="ehBox" style={{display:"inline-block",paddingLeft:15, paddingTop:50}}>
+         <font size="4">  Class of 2014 </font>
+         <br></br>
+         Degree in Medical Microbiology/ Immunology
+         <hr></hr>
+         <ul>
+          <li> 4-year Bachelor of Science program with focus on diseases and clinical diagnosis </li>
+          <li> Broad cousrework at renowned institute including math, phsyical science, literature, philosophy and ethnic studies </li>
+          <li> Rich academic culture where immersion in differing ideas, perspectives and personalities is paramount </li>
+         </ul>
+      </div>
+
+      <h2> Employment History </h2>
+      <hr></hr>
 
     </div>
     )
