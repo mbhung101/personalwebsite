260afef7b40f2ed0d8d88d83093e435a30cbc7b3
github username <github email address>
2017-12-08 16:39:48 -0500

    added files

---
+++ src/components/random.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Nav from './nav'
+export default class Random extends Component {
+
+  constructor(){
+    super()
+  }
+  render(){
+    return(
+      <div>
+      <Nav/>
+      <h1 align="center"> Random </h1>
+      <hr></hr>
+      </div>  )}
+}
